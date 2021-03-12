13a9749d6cb825456be2acbc78fdd81c1beb21a0
Kiyong Kim <scgyong.kpu@gmail.com>
2021-03-12 11:16:33 +0900

    shows Program started

--- Samples/FirstApp/app/src/main/java/kr/ac/kpu/game/s1234567/firstapp/MainActivity.java
+++ Samples/FirstApp/app/src/main/java/kr/ac/kpu/game/s1234567/firstapp/MainActivity.java
@@ -3,6 +3,7 @@ package kr.ac.kpu.game.s1234567.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.mainTextView);
+        tv.setText("Program started");
     }
 }

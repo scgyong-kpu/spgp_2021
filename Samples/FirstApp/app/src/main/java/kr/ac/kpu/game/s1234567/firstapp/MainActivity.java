712c02245e7017e166dade2f144a60567f65863f
Kiyong Kim <scgyong.kpu@gmail.com>
2021-03-12 11:26:14 +0900

    member variable 을 이용한 listener 설정

--- Samples/FirstApp/app/src/main/java/kr/ac/kpu/game/s1234567/firstapp/MainActivity.java
+++ Samples/FirstApp/app/src/main/java/kr/ac/kpu/game/s1234567/firstapp/MainActivity.java
@@ -3,6 +3,8 @@ package kr.ac.kpu.game.s1234567.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = findViewById(R.id.mainTextView);
         tv.setText("Program started");
+
+        Button helloButton = findViewById(R.id.helloButton);
+        helloButton.setOnClickListener(helloButtonListener);
     }
+
+    View.OnClickListener helloButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            TextView tv = findViewById(R.id.mainTextView);
+            tv.setText("Hello");
+        }
+    };
 }

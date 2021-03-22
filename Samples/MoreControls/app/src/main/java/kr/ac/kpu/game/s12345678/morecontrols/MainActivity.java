14456e8068051fca4a09500ee6ef37cb9f7ef134
Kiyong Kim <scgyong@gmail.com>
2021-03-22 11:49:26 +0900

    Apply 버튼 눌렀을 때 Checkbox 값 알아오기

--- Samples/MoreControls/app/src/main/java/kr/ac/kpu/game/s12345678/morecontrols/MainActivity.java
+++ Samples/MoreControls/app/src/main/java/kr/ac/kpu/game/s12345678/morecontrols/MainActivity.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox firewallCheckbox;
+    private TextView outTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firewallCheckbox = findViewById(R.id.checkbox);
+        outTextView = findViewById(R.id.outTextView);
     }
 
     public void onBtnApply(View view) {
+        boolean checked = firewallCheckbox.isChecked();
+        String text = checked ? "Using Firewall" : "Not using Firewall";
+        outTextView.setText(text);
     }
 }

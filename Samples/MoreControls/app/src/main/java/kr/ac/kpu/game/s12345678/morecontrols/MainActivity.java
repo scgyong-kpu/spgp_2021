84b308a2e294c7affb28118b4456ca5212e15344
Kiyong Kim <scgyong@gmail.com>
2021-03-22 12:01:59 +0900

    Edit 추가

--- Samples/MoreControls/app/src/main/java/kr/ac/kpu/game/s12345678/morecontrols/MainActivity.java
+++ Samples/MoreControls/app/src/main/java/kr/ac/kpu/game/s12345678/morecontrols/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox firewallCheckbox;
     private TextView outTextView;
+    private EditText userEditText;
+    private TextView editTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         firewallCheckbox = findViewById(R.id.checkbox);
         outTextView = findViewById(R.id.outTextView);
+        userEditText = findViewById(R.id.userEditText);
+        editTextView = findViewById(R.id.editTextView);
     }
 
     public void onBtnApply(View view) {
         boolean checked = firewallCheckbox.isChecked();
         String text = checked ? "Using Firewall" : "Not using Firewall";
         outTextView.setText(text);
+
+        String user = userEditText.getText().toString();
+        editTextView.setText("User info = " + user);
     }
 
     public void onCheckFirewall(View view) {

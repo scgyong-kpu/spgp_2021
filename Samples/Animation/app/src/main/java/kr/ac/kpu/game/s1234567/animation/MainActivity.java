bcbbbe386b3ca48b6b562416782cd3ffec9cc107
Kiyong Kim <scgyong.kpu@gmail.com>
2021-03-26 12:26:17 +0900

    XML 로부터 MyView 로드하기

--- Samples/Animation/app/src/main/java/kr/ac/kpu/game/s1234567/animation/MainActivity.java
+++ Samples/Animation/app/src/main/java/kr/ac/kpu/game/s1234567/animation/MainActivity.java
@@ -9,9 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        MyView mv = new MyView(this);
-        setContentView(mv);
+//        MyView mv = new MyView(this);
+//        setContentView(mv);
     }
+
 }
--- Samples/Animation/app/src/main/java/kr/ac/kpu/game/s1234567/animation/MyView.java
+++ Samples/Animation/app/src/main/java/kr/ac/kpu/game/s1234567/animation/MyView.java
@@ -4,22 +4,37 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class MyView extends View {
     private static final String TAG = MyView.class.getSimpleName();
+    private Paint paint = new Paint();
+    private Rect rect = new Rect();
 
-    public MyView(Context context) {
-        super(context);
+    public MyView(Context context, AttributeSet set) {
+        super(context, set);
+        paint.setColor(0xff0044ff);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouch: " + event);
+        return true;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Paint paint = new Paint();
-        paint.setColor(0xff0044ff);
-        Rect rect = new Rect(10, 20, 300, 400);
+        int l = 0 + getLeftPaddingOffset();
+        int t = 0 + getTopPaddingOffset();
+        int w = getWidth() - getRightPaddingOffset();
+        int h = getHeight() - getBottomPaddingOffset();
+        rect.set(l, t, w, h);
         Log.d(TAG, "drawing " + rect);
         canvas.drawRect(rect, paint);
+
     }
 }

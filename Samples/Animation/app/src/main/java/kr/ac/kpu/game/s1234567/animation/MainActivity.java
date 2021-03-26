e43d4e247bb7715e02104fc4f8bbf2c028637f96
Kiyong Kim <scgyong.kpu@gmail.com>
2021-03-26 12:34:47 +0900

    orientation change 직접 처리하기

--- Samples/Animation/app/src/main/java/kr/ac/kpu/game/s1234567/animation/MainActivity.java
+++ Samples/Animation/app/src/main/java/kr/ac/kpu/game/s1234567/animation/MainActivity.java
@@ -1,18 +1,29 @@
 package kr.ac.kpu.game.s1234567.animation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG, "onCreate");
 //        MyView mv = new MyView(this);
 //        setContentView(mv);
     }
 
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        Log.d(TAG, "onConfigChange:" + newConfig);
+        super.onConfigurationChanged(newConfig);
+    }
 }

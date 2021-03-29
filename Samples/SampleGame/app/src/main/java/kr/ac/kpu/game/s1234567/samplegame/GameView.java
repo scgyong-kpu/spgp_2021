aa3c625da030d234882a34e6f8f09a99032b9c97
Kiyong Kim <scgyong.kpu@gmail.com>
2021-03-29 11:56:03 +0900

    매 프레임 그릴때 할 일

--- Samples/SampleGame/app/src/main/java/kr/ac/kpu/game/s1234567/samplegame/GameView.java
+++ Samples/SampleGame/app/src/main/java/kr/ac/kpu/game/s1234567/samplegame/GameView.java
@@ -6,22 +6,50 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
-    private final Bitmap bitmap;
+    private static final String TAG = GameView.class.getSimpleName();
+    private Bitmap bitmap;
+
+    private float x;
+    private float y;
 
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initResources();
+        startUpdating();
+    }
+
+    private void startUpdating() {
+        doGameFrame();
+    }
+
+    private void doGameFrame() {
+//        update();
+        x += 0.01;
+        y += 0.02;
+
+//        draw();
+        invalidate();
+
+//        doGameFrame();
+    }
+
+    private void initResources() {
         Resources res = getResources();
         bitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+        x = 100;
+        y = 100;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawBitmap(bitmap, 0, 0, null);
+        canvas.drawBitmap(bitmap, x, y, null);
+        Log.d(TAG, "Drawing at: " + x + "," + y);
     }
 }
 

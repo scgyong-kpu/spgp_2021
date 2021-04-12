45e4a2922cdaef99781c5d0115d9af8899e4cdd7
Kiyong Kim <scgyong.kpu@gmail.com>
2021-04-12 10:58:11 +0900

    Frame Animation 적용

--- Samples/SampleGame/app/src/main/java/kr/ac/kpu/game/s1234567/samplegame/game/Ball.java
+++ Samples/SampleGame/app/src/main/java/kr/ac/kpu/game/s1234567/samplegame/game/Ball.java
@@ -4,6 +4,8 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.RectF;
 
 import kr.ac.kpu.game.s1234567.samplegame.framework.GameObject;
 import kr.ac.kpu.game.s1234567.samplegame.R;
@@ -14,6 +16,7 @@ public class Ball implements GameObject {
     private static int imageHeight;
     private float x, y;
     private float dx, dy;
+    private int frameIndex;
     private static Bitmap bitmap;
 
     public Ball(float x, float y, float dx, float dy) {
@@ -23,7 +26,7 @@ public class Ball implements GameObject {
         this.dy = dy;
         if (bitmap == null) {
             Resources res = GameView.view.getResources();
-            bitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+            bitmap = BitmapFactory.decodeResource(res, R.mipmap.fireball_128_24f);
             imageWidth = bitmap.getWidth();
             imageHeight = bitmap.getHeight();
         }
@@ -35,15 +38,45 @@ public class Ball implements GameObject {
         this.y += this.dy * game.frameTime;
         int w = GameView.view.getWidth();
         int h = GameView.view.getHeight();
-        if (x < 0 || x > w - imageWidth) {
+        int frameWidth = imageHeight;
+        if (x < 0 || x > w - frameWidth) {
             dx *= -1;
         }
         if (y < 0 || y > h - imageHeight) {
             dy = -dy;
         }
+
+        frameIndex = (frameIndex + 1) % 24;
     }
 
     public void draw(Canvas canvas) {
-        canvas.drawBitmap(bitmap, this.x, this.y, null);
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+//        int fw = w / 24;
+        int fw = h;
+        int ballRadius = 100;
+        Rect src = new Rect(fw * frameIndex, 0, fw * frameIndex + fw, h);
+        RectF dst = new RectF(x - ballRadius, y - ballRadius, x + ballRadius, y + ballRadius);
+        canvas.drawBitmap(bitmap, src, dst, null);
+//        canvas.drawBitmap(bitmap, this.x, this.y, null);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

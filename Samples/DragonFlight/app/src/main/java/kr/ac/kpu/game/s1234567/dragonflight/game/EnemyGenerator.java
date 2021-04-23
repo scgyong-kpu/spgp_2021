55114868c2817c4e4336260c4dad922ca4113953
Kiyong Kim <scgyong@gmail.com>
2021-04-23 11:20:50 +0900

    enemy level 에 wave 및 random 적용.

--- Samples/DragonFlight/app/src/main/java/kr/ac/kpu/game/s1234567/dragonflight/game/EnemyGenerator.java
+++ Samples/DragonFlight/app/src/main/java/kr/ac/kpu/game/s1234567/dragonflight/game/EnemyGenerator.java
@@ -10,14 +10,16 @@ import kr.ac.kpu.game.s1234567.dragonflight.ui.view.GameView;
 
 public class EnemyGenerator implements GameObject {
 
-    private static final float INITIAL_SPAWN_INTERVAL = 5.0f;
+    private static final float INITIAL_SPAWN_INTERVAL = 2.0f;
     private static final String TAG = EnemyGenerator.class.getSimpleName();
     private float time;
     private float spawnInterval;
+    private int wave;
 
     public EnemyGenerator() {
         time = INITIAL_SPAWN_INTERVAL;
         spawnInterval = INITIAL_SPAWN_INTERVAL;
+        wave = 0;
     }
     @Override
     public void update() {
@@ -30,6 +32,7 @@ public class EnemyGenerator implements GameObject {
     }
 
     private void generate() {
+        wave++;
         //Log.d(TAG, "Generate now !!");
         MainGame game = MainGame.get();
         int tenth = GameView.view.getWidth() / 10;
@@ -37,7 +40,9 @@ public class EnemyGenerator implements GameObject {
         for (int i = 1; i <= 9; i += 2) {
             int x = tenth * i;
             int y = 0;
-            int level = r.nextInt(20) + 1;
+            int level = wave / 10 - r.nextInt(3);
+            if (level < 1) level = 1;
+            if (level > 20) level = 20;
             Enemy enemy = new Enemy(level, x, y, 700);
             game.add(enemy);
         }

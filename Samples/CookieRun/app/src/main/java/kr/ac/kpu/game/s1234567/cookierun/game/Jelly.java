c1e97a26dad98b9865879c9a1a5d28d1e583449b
Kiyong Kim <scgyong@kpu.ac.kr>
2021-05-17 12:38:09 +0900

    dstRect 설정을 ImageObject 에 맡기면 안된다. 이미지 폭과 높이는 젤리 크기 만큼으로 해야 함.

--- Samples/CookieRun/app/src/main/java/kr/ac/kpu/game/s1234567/cookierun/game/Jelly.java
+++ Samples/CookieRun/app/src/main/java/kr/ac/kpu/game/s1234567/cookierun/game/Jelly.java
@@ -9,15 +9,25 @@ import kr.ac.kpu.game.s1234567.cookierun.framework.bitmap.IndexedGameBitmap;
 import kr.ac.kpu.game.s1234567.cookierun.framework.game.BaseGame;
 import kr.ac.kpu.game.s1234567.cookierun.framework.iface.GameObject;
 import kr.ac.kpu.game.s1234567.cookierun.framework.object.ImageObject;
+import kr.ac.kpu.game.s1234567.cookierun.framework.view.GameView;
 
 public class Jelly extends ImageObject {
     private static final String TAG = Jelly.class.getSimpleName();
+    public static final int JELLY_WIDTH = 66;
+    public static final int JELLY_HEIGHT = 66;
+    public static final int JELLIES_IN_A_ROW = 30;
+    public static final int BORDER_WIDTH = 2;
+    public static final int SPACING_WIDTH = 2;
     private final IndexedGameBitmap ibmp;
 
     public Jelly(int index, float x, float y) {
-        super(R.mipmap.jelly, x, y);
-        ibmp = new IndexedGameBitmap(R.mipmap.jelly, 66, 66, 30, 2, 2);
+        ibmp = new IndexedGameBitmap(R.mipmap.jelly, JELLY_WIDTH, JELLY_HEIGHT, JELLIES_IN_A_ROW, BORDER_WIDTH, SPACING_WIDTH);
         ibmp.setIndex(index);
+        float l = x - JELLY_WIDTH / 2 * GameView.MULTIPLIER;
+        float t = y - JELLY_HEIGHT / 2 * GameView.MULTIPLIER;
+        float r = x + JELLY_WIDTH / 2 * GameView.MULTIPLIER;
+        float b = y + JELLY_HEIGHT / 2 * GameView.MULTIPLIER;
+        dstRect.set(l, t, r, b);
         Log.d(TAG, "index = " + index + " Rect = " + dstRect);
     }
     @Override

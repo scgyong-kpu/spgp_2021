ecd9a85434661d4f1ac67394f1f6bb0f5e54184a
Kiyong Kim <scgyong@kpu.ac.kr>
2021-05-31 03:00:24 +0900

    화면 일부분 차지.

--- Samples/CookieRun/app/src/main/java/kr/ac/kpu/game/s1234567/cookierun/game/scenes/second/SecondScene.java
+++ Samples/CookieRun/app/src/main/java/kr/ac/kpu/game/s1234567/cookierun/game/scenes/second/SecondScene.java
@@ -5,7 +5,7 @@ import android.view.MotionEvent;
 import kr.ac.kpu.game.s1234567.cookierun.R;
 import kr.ac.kpu.game.s1234567.cookierun.framework.game.Scene;
 import kr.ac.kpu.game.s1234567.cookierun.framework.iface.GameObject;
-import kr.ac.kpu.game.s1234567.cookierun.framework.object.VerticalScrollBackground;
+import kr.ac.kpu.game.s1234567.cookierun.framework.object.ImageObject;
 import kr.ac.kpu.game.s1234567.cookierun.framework.view.GameView;
 import kr.ac.kpu.game.s1234567.cookierun.game.objs.Player;
 import kr.ac.kpu.game.s1234567.cookierun.game.scenes.main.MainGame;
@@ -25,7 +25,7 @@ public class SecondScene extends Scene {
         int h = GameView.view.getHeight();
         initLayers(Layer.COUNT.ordinal());
 
-        add(Layer.bg, new VerticalScrollBackground(R.mipmap.bg_city, -10));
+        add(Layer.bg, new ImageObject(R.mipmap.bg_city, w/2, h/2));
         add(Layer.player, new Player(w/2, h/2));
     }
 

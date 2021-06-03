4c40e8a1437620ed9f480f9d247436a57cf49b8e
Kiyong Kim <scgyong@kpu.ac.kr>
2021-06-04 07:39:46 +0900

    Clear button 추가

--- Samples/SmoothPath/app/src/main/java/kr/ac/kpu/game/agp/smoothingpath/MainActivity.java
+++ Samples/SmoothPath/app/src/main/java/kr/ac/kpu/game/agp/smoothingpath/MainActivity.java
@@ -3,12 +3,20 @@ package kr.ac.kpu.game.agp.smoothingpath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    PathView pathView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pathView = findViewById(R.id.pathView);
+    }
+
+    public void onBtnClear(View view) {
+        pathView.clear();
     }
 }
--- Samples/SmoothPath/app/src/main/java/kr/ac/kpu/game/agp/smoothingpath/PathView.java
+++ Samples/SmoothPath/app/src/main/java/kr/ac/kpu/game/agp/smoothingpath/PathView.java
@@ -72,4 +72,9 @@ public class PathView extends View {
     }
 
     Point x;
+
+    public void clear() {
+        points.clear();
+        invalidate();
+    }
 }

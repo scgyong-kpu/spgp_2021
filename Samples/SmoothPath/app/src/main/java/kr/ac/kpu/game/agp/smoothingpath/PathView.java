a7ecbe185799aab9da5ea67f2529fc74e4f53e56
Kiyong Kim <scgyong@kpu.ac.kr>
2021-06-04 07:16:24 +0900

    View 기본

---
+++ Samples/SmoothPath/app/src/main/java/kr/ac/kpu/game/agp/smoothingpath/PathView.java
@@ -0,0 +1,16 @@
+package kr.ac.kpu.game.agp.smoothingpath;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class PathView extends View {
+    public PathView(Context context) {
+        super(context);
+    }
+    public PathView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
